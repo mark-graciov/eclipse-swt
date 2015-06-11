26848a7e844d024f217b7c0a1e2675c305976918
Mihail Racu <racu.mihail@gmail.com>
2015-06-11 14:29:49 +0300

    -Now you can see validation image info when button is pressed

--- com.mracu.plugin.regex.validator/src/com/mracu/plugin/regex/validator/RegexValidatorView.java
+++ com.mracu.plugin.regex.validator/src/com/mracu/plugin/regex/validator/RegexValidatorView.java
@@ -1,7 +1,7 @@
 package com.mracu.plugin.regex.validator;
 
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
@@ -15,13 +15,13 @@ import org.eclipse.ui.forms.widgets.TableWrapLayout;
 import org.eclipse.ui.part.ViewPart;
 
 public class RegexValidatorView extends ViewPart {
-	private FormToolkit toolkit;
-	private Form form;
 	private static final String[] ICONS = { "icons/no_text.png",
 			"icons/valid.png", "icons/not_valid.png" };
-	private ImageDescriptor ICON;
-
-	// private Label imageLabel;
+	private FormToolkit toolkit;
+	private Form form;
+	Label imageLabel;
+	Text patternText;
+	Text valueText;
 
 	@Override
 	public void createPartControl(Composite parent) {
@@ -31,56 +31,56 @@ public class RegexValidatorView extends ViewPart {
 		TableWrapLayout layout = new TableWrapLayout();
 		form.getBody().setLayout(layout);
 		layout.numColumns = 2;
+
 		TableWrapData td = new TableWrapData();
 		td.colspan = 2;
 		Label patternLabel = toolkit.createLabel(form.getBody(), "Pattern");
-		final Text patternText = toolkit.createText(form.getBody(), "");
+		patternText = toolkit.createText(form.getBody(), "");
 		td = new TableWrapData(TableWrapData.FILL_GRAB);
 		patternText.setLayoutData(td);
 
 		Label valueLabel = toolkit.createLabel(form.getBody(), "Value");
-		final Text valueText = toolkit.createText(form.getBody(), "");
+		valueText = toolkit.createText(form.getBody(), "");
 		td = new TableWrapData(TableWrapData.FILL_GRAB);
 		valueText.setLayoutData(td);
 
-		
-		final Label imageLabel = toolkit.createLabel(form.getBody(),
-				"dsdasdasd");
+		td = new TableWrapData(TableWrapData.FILL);
+		td.heightHint = 32;
+		imageLabel = toolkit.createLabel(form.getBody(), "");
+		imageLabel.setLayoutData(td);
+
 		Button validateButton = toolkit.createButton(form.getBody(),
 				"Validate", SWT.PUSH);
 		td = new TableWrapData(TableWrapData.RIGHT);
 		validateButton.setLayoutData(td);
-		td.colspan = 2;
+
 		validateButton.addListener(SWT.Selection, new Listener() {
 
 			public void handleEvent(Event event) {
-				if (patternText.getText() == "" && valueText.getText() == "") {
-					ICON = Activator.getImageDescriptor(ICONS[0]);
-					imageLabel.setImage(ICON.createImage());
-					System.out.println("Input is empty!");
-					System.out.println("Please type a text!");
-				} else {
-
-					RegexValdidator regexValdidator = new RegexValdidator();
-
-					regexValdidator.setPatternWord(patternText.getText());
-					regexValdidator.setExpWord(valueText.getText());
-
-					boolean isValid = regexValdidator.validateRegex();
-					if (isValid) {
-						ICON = Activator.getImageDescriptor(ICONS[1]);
-						imageLabel.setImage(ICON.createImage());
-						System.out.println("Expression is valid!");
-					} else {
-						ICON = Activator.getImageDescriptor(ICONS[2]);
-						imageLabel.setImage(ICON.createImage());
-						System.out.println("Expression is not valid!");
-					}
-
-				}
-
+				drawValidation();
 			}
+
 		});
+	}
+
+	public void drawValidation() {
+		if (patternText.getText() == "" && valueText.getText() == "") {
+			imageLabel.setImage(new Image(form.getDisplay(), ResourceLoader
+					.load(ICONS[0])));
+		} else {
+			RegexValdidator regexValdidator = new RegexValdidator();
+			regexValdidator.setPatternWord(patternText.getText());
+			regexValdidator.setExpWord(valueText.getText());
+
+			boolean isValid = regexValdidator.validateRegex();
+			if (isValid) {
+				imageLabel.setImage(new Image(form.getDisplay(), ResourceLoader
+						.load(ICONS[1])));
+			} else {
+				imageLabel.setImage(new Image(form.getDisplay(), ResourceLoader
+						.load(ICONS[2])));
+			}
+		}
 	}
 
 	@Override
